0224c4ea5ff496a5f5602b9cc333e1a7379f7b2c
Dayana <bentancorday@gmail.com>
2024-03-17 22:07:21 -0300

    primera entrega finalizada

---
+++ main.js
@@ -0,0 +1,44 @@
+const pedido = [];
+
+let total = 0;
+
+let comprar = confirm("¿Deseas hacer un pedido?");
+
+if (comprar) {
+    let menu;
+    do {
+        // No se como hacer para que me aparezca como una lista, pero trate de separar para que se entienda el menú
+      menu = prompt("Nuestro menú incluye:  1-Milanesa Napolitana $250   2-Pizza $200      3-Hamburguesa $180     4-Chivito $300             Por favor, ingresa el número correspondiente al menú que deseas:");
+
+        switch(menu) {
+            case "1":
+                pedido.push({nombre: "Milanesa Napolitana", precio: 250});
+                total += 250;
+                break;
+            case "2":
+                pedido.push({nombre: "Pizza", precio: 200});
+                total += 200;
+                break;
+            case "3":
+                pedido.push({nombre: "Hamburguesa", precio: 180});
+                total += 180;
+                break;
+            case "4":
+                pedido.push({nombre: "Chivito", precio: 300});
+                total += 300;
+                break;
+            default:
+                alert("Lo siento! Ese artículo no está incluido en el menú.");
+        }
+
+        comprar = confirm("¿Deseas agregar algo más al pedido?");
+        
+    } while (comprar);
+}
+
+if (pedido.length > 0) {
+  
+    alert("Total a pagar: $" + total);
+} else {
+    alert("No se realizó ningún pedido.");
+}
